f99020ce74ceb10a00710246c1ddd216eba0787d
srimahalakshmi55 <srimahalakshmiboppana55@gmail.com>
2026-06-26 23:05:04 +0530

    reverse number program in reverse.java

---
+++ Javabasics/reverse.java
@@ -0,0 +1,16 @@
+package basic;
+import java.util.Scanner;
+public class reverse {
+	public static void main(String[] args) {
+	Scanner sc=new Scanner(System.in);
+	System.out.println("enter the number:");
+	int n=sc.nextInt();
+	
+	int rev=0;
+	while(n!=0) {
+		rev=rev*10+(n%10);     //% gives the reminder
+		n=n/10;   //  "/" gives the quotient
+	}
+	System.out.println("reverse of the number is:"+rev);
+}
+}
