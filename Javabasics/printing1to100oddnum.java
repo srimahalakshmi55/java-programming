30b57dd1184da85e41c202a4126df6b9fa5de198
srimahalakshmi55 <srimahalakshmiboppana55@gmail.com>
2026-07-14 22:46:21 +0530

    Add Java program to print odd numbers from 1 to 100

---
+++ Javabasics/printing1to100oddnum.java
@@ -0,0 +1,14 @@
+package basic;
+public class printing1to100oddnum {
+
+	public static void main(String[] args) {
+		for(int i=1;i<=100;i++) {
+			if(i%2!=0) {
+				System.out.print(i+",");
+			}
+			
+		}
+
+	}
+
+}
