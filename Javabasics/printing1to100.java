04b70cebac344f1340fa4ed0a69e0eaafc3d7f53
srimahalakshmi55 <srimahalakshmiboppana55@gmail.com>
2026-07-14 22:41:10 +0530

    Add printing1to100.java to print numbers 1 to 100

---
+++ Javabasics/printing1to100.java
@@ -0,0 +1,15 @@
+package basic;
+
+public class printing1to100 {
+
+	public static void main(String[] args) {
+		for(int i=1;i<101;i++) {
+			if(i==100) {
+				System.out.println(i);
+			}
+			else
+			System.out.print(i+",");
+		}
+	}
+
+}
